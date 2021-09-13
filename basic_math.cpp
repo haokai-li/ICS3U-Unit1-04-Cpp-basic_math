e3f7be83cc2513a72a7b92c7597a1f4a47faba1f
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-09-13 04:19:43 +0000

    new 1

---
+++ basic_math.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2021 haokai All rights reserved
+//
+//
+// Created by: Haokai
+// Created on: Sept 2021
+// This is the "basic math" program, with proper style
+
+#include <iostream>
+
+int main() {
+	std::cout << "A rectangle with dimensions 5cm & 3cm." << std::endl;
+	std::cout << "Calculate the area and perimeter." << std::endl;
+	std::cout << "" << std::endl;
+	std::cout << "a = 5 x 3 = " << (5 * 3) << "cm²" << std::endl;
+	std::cout << "p = (5 + 3) x 2 = " << ((5 + 3) * 2) << "cm" << std::endl;
+}
