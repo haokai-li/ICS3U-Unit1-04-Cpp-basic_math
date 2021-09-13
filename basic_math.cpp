16bad3acdf2c8c8b3da44005beb7a738f0a02ffb
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-09-13 17:49:12 +0000

    new 3

--- basic_math.cpp
+++ basic_math.cpp
@@ -10,6 +10,6 @@ int main() {
   std::cout << "A rectangle with dimensions 5cm & 3cm." << std::endl;
   std::cout << "Calculate the area and perimeter." << std::endl;
   std::cout << "" << std::endl;
-  std::cout << "a = 5 x 3 = " << (5 * 3) << "cm²" << std::endl;
-  std::cout << "p = (5 + 3) x 2 = " << ((5 + 3) * 2) << "cm" << std::endl;
+  std::cout << "a = 5 x 3 = " << (5 * 3) << " cm²" << std::endl;
+  std::cout << "p = (5 + 3) x 2 = " << ((5 + 3) * 2) << " cm" << std::endl;
 }
